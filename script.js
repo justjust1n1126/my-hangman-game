590ea7563d85219f8586a1c3621529181aec9dc6
justjust1n1126 <75755009+justjust1n1126@users.noreply.github.com>
2024-09-29 19:45:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// DOM Elements
+
 const elements = {
   letterContainer: document.getElementById("letter-container"),
   optionsContainer: document.getElementById("options-container"),
@@ -9,7 +9,6 @@ const elements = {
   optionsButtons: document.getElementById("options-buttons")
 };
 
-// Game options
 const options = {
   fruits: [
     "Apple", "Blueberry", "Mandarin", "Pineapple", "Pomegranate", "Watermelon",
@@ -53,4 +52,59 @@ const blocker = () => {
   elements.newGameContainer.classList.remove("hide");
 };
 
-const generateWord = (optionValue
+const generateWord = (optionValue) => {
+  elements.letterContainer.classList.remove("hide");
+  elements.userInputSection.innerText = "";
+
+  let optionArray = options[optionValue];
+  chosenWord = optionArray[Math.floor(Math.random() * optionArray.length)].toUpperCase();
+  
+  let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>');
+  elements.userInputSection.innerHTML = displayItem;
+};
+
+const initializer = () => {
+  winCount = 0;
+  count = 0;
+
+  elements.userInputSection.innerHTML = "";
+  elements.letterContainer.classList.add("hide");
+  elements.newGameContainer.classList.add("hide");
+  elements.letterContainer.innerHTML = "";
+
+  for (let i = 65; i < 91; i++) {
+    let button = document.createElement("button");
+    button.classList.add("letters");
+    button.innerText = String.fromCharCode(i);
+    button.addEventListener("click", () => {
+      let charArray = chosenWord.split("");
+      let dashes = document.getElementsByClassName("dashes");
+
+      if (charArray.includes(button.innerText)) {
+        charArray.forEach((char, index) => {
+          if (char === button.innerText) {
+            dashes[index].innerText = char;
+            winCount += 1;
+            if (winCount == charArray.length) {
+              elements.resultText.innerHTML = `<h2 class='win-msg'>You Win!!</h2><p>The word was <span>${chosenWord}</span></p>`;
+              blocker();
+            }
+          }
+        });
+      } else {
+        count += 1;
+        if (count == 6) {
+          elements.resultText.innerHTML = `<h2 class='lose-msg'>You Lose!!</h2><p>The word was <span>${chosenWord}</span></p>`;
+          blocker();
+        }
+      }
+      button.disabled = true;
+    });
+    elements.letterContainer.append(button);
+  }
+
+  displayOptions();
+};
+
+elements.newGameButton.addEventListener("click", initializer);
+window.onload = initializer;
